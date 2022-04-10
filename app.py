3ce3aa72feccbc6175c700a3bde511be4c810426
Rishi Khan <rishi.rkhan@gmail.com>
2022-04-10 14:08:05 -0400

    Added logging to find error in prod

--- app.py
+++ app.py
@@ -87,4 +87,7 @@ def scrape_results():
     return df
 
 if __name__ == '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     app.run()  # run our Flask app
