789fc58f94e978c6e47f91929758faa084a938f3
Rishi Khan <rishi.rkhan@gmail.com>
2022-04-11 21:22:25 -0400

    Added spreadsheet update and cleaned code with threading

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+from app import app
+
+if __name__ == '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    app.run()  # run our Flask app
